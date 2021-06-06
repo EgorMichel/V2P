02cf6dd12ee5d3fc1d15cc782dbb613c248b19bf
emihel <e.mihel@yandex.ru>
2021-06-07 00:56:32 +0300

    Changes potential

--- Three-dimensional gas.py
+++ Three-dimensional gas.py
@@ -6,10 +6,13 @@ pygame.init()
 population = 100
 
 Helium_diam_absolut = 2.1 * 10 ** (-10)  # meters
-meters_in_pixel = Helium_diam_absolut / 8  # 2,625 * 10 ** (-11)
+meters_in_pixel = 2 * 10 ** (-12)  # 2,625 * 10 ** (-11)
 Helium_diam_relative_max = 8
-dt = 10 ** (-13)  # seconds
+dt = 10 ** (-14)  # seconds
 initial_velocity = 1000  # meters / second
+k_bolzman = 1.38 * 10 ** (-23)
+eps_potential = 10.22 * k_bolzman  # Joules
+sigma_potential = 2.556 * 10 ** (-10)  # meters
 
 width = 800
 height = 800
@@ -17,7 +20,7 @@ depth = 800
 
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-FPS = 90
+FPS = 60
 running = True
 
 G = 6.6743 * 10 ** (-11)  # (-11)
@@ -52,7 +55,8 @@ def render():
     screen.fill((20, 5, 40))
     for molecule in molecules:
         constant = 255 / (initial_velocity ** 2 * 3) ** 0.5
-        red = constant * abs((molecule.velocity[0] ** 2 + molecule.velocity[1] ** 2 + molecule.velocity[2] ** 2) ** (1 / 2))
+        red = constant * abs(
+            (molecule.velocity[0] ** 2 + molecule.velocity[1] ** 2 + molecule.velocity[2] ** 2) ** (1 / 2))
         if red > 255:
             red = 255
         molecule.draw((red, 0, 255 - red))
@@ -67,14 +71,28 @@ def update():
     for molecule in molecules:
         for another_m in molecules:
             if another_m.x != molecule.x and another_m.y != molecule.y and another_m.z != molecule.z:
-                molecule.velocity[0] += G * another_m.m * (another_m.x - molecule.x) * dt \
-                                        / ((distance(molecule, another_m)) ** 3)
+                # molecule.velocity[0] += G * another_m.m * (another_m.x - molecule.x) * dt \
+                #                         / ((distance(molecule, another_m)) ** 3)
+                #
+                # molecule.velocity[1] += G * another_m.m * (another_m.y - molecule.y) * dt \
+                #                         / ((distance(molecule, another_m)) ** 3)
+                #
+                # molecule.velocity[2] += G * another_m.m * (another_m.z - molecule.z) * dt \
+                #                         / ((distance(molecule, another_m)) ** 3)
+                molecule.velocity[0] += (-48 * eps_potential * sigma_potential ** 12
+                                         + 24 * eps_potential * sigma_potential ** 6
+                                         * distance(molecule, another_m) ** 6) \
+                                        * (another_m.x - molecule.x) / (distance(molecule, another_m) ** 14)
 
-                molecule.velocity[1] += G * another_m.m * (another_m.y - molecule.y) * dt \
-                                        / ((distance(molecule, another_m)) ** 3)
+                molecule.velocity[0] += (-48 * eps_potential * sigma_potential ** 12
+                                         + 24 * eps_potential * sigma_potential ** 6
+                                         * distance(molecule, another_m) ** 6) \
+                                        * (another_m.y - molecule.y) / (distance(molecule, another_m) ** 14)
 
-                molecule.velocity[2] += G * another_m.m * (another_m.z - molecule.z) * dt \
-                                        / ((distance(molecule, another_m)) ** 3)
+                molecule.velocity[0] += (-48 * eps_potential * sigma_potential ** 12
+                                         + 24 * eps_potential * sigma_potential ** 6
+                                         * distance(molecule, another_m) ** 6) \
+                                        * (another_m.z - molecule.z) / (distance(molecule, another_m) ** 14)
 
         molecule.x += molecule.velocity[0] * dt
         molecule.y += molecule.velocity[1] * dt
